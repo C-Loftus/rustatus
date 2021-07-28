379a8bd5121e143d1661305d4d1118a9e61ccbb8
colton <cloftus@princeton.edu>
2021-07-28 18:31:51 -0400

    Added optional IP status cmd's

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 use std::process::{Command, Stdio};
 use chrono::Local;
 use std::fs;
+use ipgeolocate::{Locator, Service};
+
+/********************* */
+// Options aren't used in these functions. Instead error handling is
+// just done within the function itself, i.e. by returning a blank string
+// or a string that is formatted but just has no number
+
+// It is easier to handle each case in the functions since each one is
+// different. Updating the String to a long error is rarely desirable,
+// since it will cause the hostname to become much longer and thus 
+// make the status bar unpleasing to the eye.
+/********************* */
 
 pub fn awk_volume() -> String {
     let status = Command::new("amixer")
@@ -32,16 +44,16 @@ pub fn awk_volume() -> String {
 
     if vec.len() >= 2 {
         if vec[0] == vec[1] {
-            return String::from(format!(" Vol: {}% | ", vec[0]))
+            return String::from(format!(" V: {}% | ", vec[0]))
         }
         else {
             // linux updates one side faster when updating both, 
             //unbalance is always corrected after a second passes
-            return String::from(" Vol: U% | ")
+            return String::from(" V: U% | ")
         }
     }
     else {
-        return String::from("Err")
+        return String::from(" V: E% | ")
     }
 }
 
@@ -56,35 +68,39 @@ pub fn wifi_name() -> String{
     let temp = String::from_utf8_lossy(&output.stdout).into_owned()
     .replace(|c: char| !c.is_ascii_alphanumeric(), "");
 
-    return String::from(format!("Net: {} | ", temp))
+    return String::from(format!("N: {} | ", temp))
 }
 
 pub fn time() -> String {
     return Local::now().format("%I:%M %b-%d-%Y ").to_string();
 }
 pub fn mouse_bat() -> String {
-    let contents = fs::read_to_string("/sys/class/power_supply/hidpp_battery_1/capacity_level");
+    let contents = fs::read_to_string("/sys/class/power_supply/hidpp_battery_0/capacity_level");
     let output = match contents {
         Ok(file) => file,
-        Err(_) => String::from(""),
+        // backup battery location is not located at default spot
+        Err(_) => match fs::read_to_string("/sys/class/power_supply/hidpp_battery_1/capacity_level"){
+            Ok(file2) => file2,
+            Err(_) => String::from("")
+        },
     }
     .replace(|c: char| !c.is_ascii_alphanumeric(), "");
 
-    let charging_status = fs::read_to_string("/sys/class/power_supply/hidpp_battery_1/status");
+    let charging_status = fs::read_to_string("/sys/class/power_supply/hidpp_battery_0/status");
     match charging_status {
         Ok(file) => {
             if output == String::from("") {
-                return String::from(format!("MB: {} | ", file))
+                return String::from(format!("MB: {} | ", file)) 
             }
         },
-
-        Err(_) => (),
-        // uncomment if you want a blank string instead of a formatted one 
-        // with no value
-        
-        /* if output == String::from("") {
-             return String::from("")
-         }, */
+        Err(_) => match fs::read_to_string("/sys/class/power_supply/hidpp_battery_1/status"){
+            Ok(file2) => {
+                if output == String::from("") {
+                    return String::from(format!("MB: {} | ", file2))
+                }
+            },
+            Err(_) => (),
+        },
     };
     return String::from(format!("MB: {} | ", output))
 }
@@ -110,7 +126,27 @@ pub fn internal_bat() -> String {
 }
 
 
+pub fn get_ip() -> String {
+    let output = Command::new("dig +short myip.opendns.com @resolver1.opendns.com")
+    .output();
+    let output = match output {
+        Ok(file) => file,
+        Err(_) => return String::from(""),
+    };
+    let temp = String::from_utf8_lossy(&output.stdout).into_owned()
+    .replace(|c: char| !c.is_ascii_alphanumeric(), "");
 
+    return temp.to_string()
+}
+
+pub async fn location(ip: &str) -> (String, String) {
+    let service = Service::IpApi;
+    match Locator::get(ip, service).await {
+        Ok(ip) => return (ip.city, ip.country),
+        Err(_) => return (String::from("Undef City"),
+                          String::from("Undef Country")),
+    };
+}
 
 // not so much tests as instead quick ways to display output
 // TODO turn into proper tests. 
@@ -135,5 +171,10 @@ mod tests {
     #[test]
     fn mb_test() {
         print!("\n{}\n", mouse_bat());
+    }
+    #[test]
+    fn location_test() {
+        let locale = location(&get_ip()).await;
+        print!("\n{}\n", locale);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use std::process::Command;
 mod lib;
 use lib::*;
 use std::thread;
-use std::time::Duration;
+use std::io::Write;
+use std::io::stdout;
 
 fn main() {
-
+    print!("starting");
     loop {
         let volume = awk_volume();
         let wifi = wifi_name();
@@ -14,13 +15,14 @@ fn main() {
         let bat = internal_bat();
 
         let output = volume + &wifi + &mb + &bat + &time;
-        // print!("{}", output);
-        // bat, mouse bat, usb conn, ip, brightness
+        print!("\n{}", output);
+        // stdout().flush().unwrap();
+        // // bat, mouse bat, usb conn, ip, brightness
         Command::new("xsetroot")
         .arg("-name")
         .arg(output.to_string())
         .spawn()
         .expect("xsetroot command failed to start");
-        thread::sleep(std::time::Duration::from_millis(100));
+        thread::sleep(std::time::Duration::from_millis(1000));
         }
     }
